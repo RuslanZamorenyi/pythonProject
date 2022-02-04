d680403fdfe2fe985a97d63686ad97b014374719
Ruslan <rzamorenyj@gmail.com>
2022-02-04 21:40:51 +0300

    task

---
+++ main.py
@@ -0,0 +1,34 @@
+# x = [11,5,8,32,15,3,20,132,21,4,555,9,20]
+# sums = 0
+# for y in x:
+#   if y<3000:
+#     if y%3==0:
+#       print (y)
+#       sums = sums+y
+# print(sums)
+#
+#
+#
+for x in range(0,24):
+  for y in range(0,60):
+    for z in range (0,60):
+      print(f"{x}:{y}:{z}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
