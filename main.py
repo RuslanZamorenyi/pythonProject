09040cfc345558b23c73a8b470b8f930ffbd66fe
Ruslan <rzamorenyj@gmail.com>
2022-02-09 21:08:06 +0300

    task

--- main.py
+++ main.py
@@ -9,10 +9,74 @@
 #
 #
 #
-for x in range(0,24):
-  for y in range(0,60):
-    for z in range (0,60):
-      print(f"{x}:{y}:{z}")
+# for x in range(0,24):
+#   for y in range(0,60):
+#     for z in range (0,60):
+#       print(f"{x}:{y}:{z}"
+
+
+# for x in range(1, 999):
+#   if x%2==0:
+#     print(x)
+
+# sums = 0
+# for i in range(1,100):
+#   sums=sums+i
+# print(sums)
+# list = [2, 5, -88]
+# count = 0
+# for i in list:
+#   if i>0:
+#     count = count+1
+# print(count)
+
+# sums = 1
+# for i in range(1,7):
+#   sums = sums*i
+# print(sums)
+
+# for i in range(1,10):
+#   for e in range(1,10):
+#     print(i*e,end = "\n")
+
+# a =1
+# for i in range(1,11):
+#     b=1
+#     for e in range(1,11):
+#         rez=a*b
+#         print(rez, end=" ")
+#         b+=1
+#     print(' ')
+#     a+=1
+
+
+
+# def fun(star, stop, step):
+#     list_bl = []
+#     for i in range(star, stop, step):
+#         list_bl.append(i)
+#     return list_bl
+# fun(24, 2, 4)
+# print(fun(24, 2, -4))
+
+list = [11,5 8,32, 15, 3]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
