5057082f94e98bb3249a96ae3385b55319ac652b
Ruslan <rzamorenyj@gmail.com>
2022-02-20 21:04:59 +0300

    task

--- main.py
+++ main.py
@@ -47,11 +47,11 @@
 #         print(rez, end=" ")
 #         b+=1
 #     print(' ')
-#     a+=1
+    # a+=1
 
 
 
-# def fun(star, stop, step):
+# def fun(a, b, c):
 #     list_bl = []
 #     for i in range(star, stop, step):
 #         list_bl.append(i)
@@ -59,7 +59,82 @@
 # fun(24, 2, 4)
 # print(fun(24, 2, -4))
 
-list = [11,5 8,32, 15, 3]
+# list = [11,5,8,32,15,3,20,132,21,4,555,9,20]
+#
+# def fun(less, share, list):
+#     list_bl = []
+#     for i in list:
+#         if i <= less:
+#             if i%share==0:
+#                 list_bl.append(i)
+#     return list_bl
+# print(fun(30, 3, list))
+# sums = 0
+# def fun(one, two):
+#     sums = 0
+#     for i in range(one, two):
+#         sums = sums+i
+#     return sums
+# print(fun(1, 100))
+
+# list = [3, -3, 8, -23]
+# def fun(list):
+#     count = 0
+#     for i in list:
+#         if i>=0:
+#             count = count+1
+#     return count
+# print(fun(list))
+# def fun(one, two):
+#     list = []
+#     for i in range(one, two):
+#         if i%2==0:
+#             list.append(i)
+#     return list
+# print(fun(1, 99))
+
+
+# def fun(start, end):
+#     sums = 1
+#
+#     for i in range(start, end):
+#         sums = sums*i
+#     return sums
+# print(fun(1, 7))
+# def fun(one):
+#     a = 1
+#     for i in range(1, one):
+#         b=1
+#         for e in range(1, one):
+#             rez=a*b
+#             print(rez, end=" ")
+#             b+=1
+#         print(' ')
+#         a+=1
+
+# fun(11)
+ list = [26, 45, 85, 66, 45, 62]
+ print(list[0])
+ print(list[-1])
+
+a = 4
+b = 7
+с = 0
+c = a
+a = b
+b = c
+print(a, b)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
